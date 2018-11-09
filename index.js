45de66e74477af0c9950bdc435bd7f9c34d06795
ChrisGermano <chris.eg.germano@gmail.com>
2018-11-08 19:31:07 -0500

    Fix NeDB update

--- index.js
+++ index.js
@@ -70,9 +70,9 @@ function updateTimer() {
             if (docs.length === 0) {
               console.log("NO WINNER ASSOCIATED ERROR");
             } else {
-              db.update({ _id : chosen.user }, { score : docs[0].score + 1 } function(err, docs) {
+              db.update({ _id : chosen.user }, { $set: { score : docs[0].score + 1 }, {}, function(err, docs) {
                 console.log("UPDATED?");
-                console.log(docs[0]);
+                console.log(docs);
               })
             }
           }
